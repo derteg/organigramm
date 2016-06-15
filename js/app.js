85218e568a37a41b2d12eea3198b7dc74a5bc58a
Alexey Dibrivnyy <i.ya.kadet@gmail.com>
2016-06-15 18:45:11 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
     function Organigramm () {
         var self = this,
             workspace = document.getElementById('organigram'),
-            canvas = workspace.querySelector('.organigram__canvas');
+            canvas = $('.organigram__canvas').get(0);
 
         this.config = {
             thatEl: '',
